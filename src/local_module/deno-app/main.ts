d2f64ae8d4cdbdf7bc7caf1daf0e23e264e4fdfc
Abhayraj Yadav <abhayraj.yadav.st4@staytuned.company>
2025-01-31 13:53:31 +0530

    Add UUID generation to hello function and update dependencies
    local_module | #2 | dev

--- src/local_module/deno-app/main.ts
+++ src/local_module/deno-app/main.ts
@@ -1,8 +1,11 @@
-import { createRequire } from "node:module";
-const require = createRequire(import.meta.url);
+// import { createRequire } from "node:module";
+// const require = createRequire(import.meta.url);
 
-// Provide the absolute path to your local `abc` package
-const { hello, processText } = require("../abc/index.js");
+// // Provide the absolute path to your local `abc` package
+// const { hello, processText } = require("../abc/index.js");
+
+
+import { hello, processText } from "abc"
 
 console.log( hello());
 console.log("Processing Text from abc:", processText("test string to process"));
