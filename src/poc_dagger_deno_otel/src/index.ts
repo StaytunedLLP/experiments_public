dbacf336d675a34635c4e698a48915f2a2f8532e
aditi-shah-st1 <aditi.shah.st1@staytuned.company>
2025-03-27 15:06:00 +0530

    converted example with deno

--- src/poc_dagger_deno_otel/src/index.ts
+++ src/poc_dagger_deno_otel/src/index.ts
@@ -1,42 +1,45 @@
+/**
+ * A generated module for PocDaggerDenoOtel functions
+ *
+ * This module has been generated via dagger init and serves as a reference to
+ * basic module structure as you get started with Dagger.
+ *
+ * Two functions have been pre-created. You can modify, delete, or add to them,
+ * as needed. They demonstrate usage of arguments and return types using simple
+ * echo and grep commands. The functions can be called from the dagger CLI or
+ * from one of the SDKs.
+ *
+ * The first line in this comment block is a short description line and the
+ * rest is a long description with more detail on the module's purpose or usage,
+ * if appropriate. All modules should have a short description.
+ */
 import {
   Container,
   dag,
   Directory,
   func,
   object,
-  Service,
+  type Service,
 } from "@dagger.io/dagger";
 
 @object()
-export class OtelDagger {
+export class PocDaggerDenoOtel {
+  /**
+   * Returns a container that echoes whatever string argument is provided
+   */
   @func()
-  denoRunWithEnv(source: Directory): Promise<Service> {
-    const service = dag
-      .container()
-      .from("denoland/deno:latest")
-      .withDirectory("/app", source)
-      .withWorkdir("/app")
-      // .withEnvVariable("OTEL_EXPORTER_OTLP_PROTOCOL", "http/json")
-      .withEnvVariable("OTEL_DENO", "true")
-      .withEnvVariable("OTEL_DENO_CONSOLE", "true")
-      .withExec(["deno", "task", "start"])
-      .withExposedPort(8800)
-      .asService();
-    return service;
+  containerEcho(stringArg: string): Container {
+    return dag.container().from("alpine:latest").withExec(["echo", stringArg]);
   }
 
-  @func()
-  denoRunWithoutEnv(source: Directory): Promise<Service> {
-    const service = dag
-      .container()
-      .from("denoland/deno:latest")
+  async denoRunWithoutEnv(source: Directory): Promise<Service> {
+    return dag.container().from("denoland/deno:latest")
       .withDirectory("/app", source)
       .withWorkdir("/app")
       .withEnvVariable("OTEL_DENO", "true")
       .withExec(["deno", "task", "start-with-env"])
       .withExposedPort(8800)
       .asService();
-    return service;
   }
 
   @func()
